83efb8a884500227b66276b589ca449baba452d5
Zen Shane Chia <zenshanec@gmail.com>
2020-09-15 14:46:26 -0600

    Files going in

---
+++ Mork/Assets/Scenes/Shane's Testing/Scripts/LoadingScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class LoadingScene : MonoBehaviour
+{
+    [SerializeField]
+    private Image progressBar;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(loadingNextScene());
+    }
+
+    IEnumerator loadingNextScene()
+    {
+        AsyncOperation loadScene = SceneManager.LoadSceneAsync(2);
+        while(loadScene.progress < 1)
+        {
+            progressBar.fillAmount = loadScene.progress;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
---
+++ Mork/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    //Initialize some variables
+    public CharacterController controller;
+
+    public Transform camera;
+
+    public float speed = 6f;
+
+    public float smoothTurnTime = 0.2f;
+    float turnSmoothVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get the player input
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(horizontalMovement, 0f, verticalMovement).normalized;
+
+        //If the player pressed anymovement keys
+        if (movement.magnitude >= 0.1f)
+        {
+            //Calculate the angle and distance of that movement
+            float targetAngle = Mathf.Atan2(movement.x,movement.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y,targetAngle, ref turnSmoothVelocity,smoothTurnTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            //Make the object move
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+    }
+}
