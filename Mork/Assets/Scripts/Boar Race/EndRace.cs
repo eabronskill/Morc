066ca6ff525f1d59c40a61bd40d4e65b8f048151
eabronskill <eabronskill2017@gmail.com>
2020-09-17 13:58:35 -0600

    End Race Trigger
    - Did not test uwu

---
+++ Mork/Assets/Scripts/Boar Race/EndRace.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndRace : LoadingScene
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Boar"))
+        {
+            loadingNextScene(nextScene);
+        }
+    }
+}
