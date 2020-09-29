9ab4399a415b86f28703634ec2a02214642bcd22
MatthewTroutman <matthew@troutman.us>
2020-09-29 13:28:44 -0600

    Dialogue updated

--- Mork/Assets/Scripts/Dialogue/NpcTarget.cs
+++ Mork/Assets/Scripts/Dialogue/NpcTarget.cs
@@ -8,14 +8,14 @@ public class NpcTarget : MonoBehaviour
 {
     private void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.F))
+        if (col.gameObject.tag == "Player" && Input.GetKey(KeyCode.F))
         {
             this.SendMessage("TriggerDialogue");
         }
     }
     private void OnTriggerStay(Collider col)
     {
-        if (col.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.F))
+        if (col.gameObject.tag == "Player" && Input.GetKey(KeyCode.F))
         {
             this.SendMessage("TriggerDialogue");
             Console.WriteLine("Got into Dialogue");
